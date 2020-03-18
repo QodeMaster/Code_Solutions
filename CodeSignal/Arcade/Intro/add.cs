35f56b9c46625766e5b111679165c05d66ca1cce
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-03-18 10:33:23 +0100

    Update add.cs

--- CodeSignal/Arcade/Intro/add.cs
+++ CodeSignal/Arcade/Intro/add.cs
@@ -2,6 +2,8 @@ int add(int param1, int param2) {
   return param1 + param2;
 }
 
+/*--------------------------------------------------------*/
+/*--------------------------------------------------------*/
 
 /* You can also make the method a one-liner */
-// int add(int param1, int param2) => param1 + param2;
+int add(int param1, int param2) => param1 + param2;
