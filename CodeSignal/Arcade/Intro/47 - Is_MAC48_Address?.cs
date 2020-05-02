d81c1848440673021e301168e373f823190316c8
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-05-02 09:48:49 +0200

    Create 47 - Is_MAC48_Address?.cs

---
+++ CodeSignal/Arcade/Intro/47 - Is_MAC48_Address?.cs
@@ -0,0 +1,24 @@
+// Basic Solution
+
+bool isMAC48Address(string inputString) {
+    Regex reg = new Regex(@"[A-F\d-]{14}-[A-F\d]{2}");
+    if(reg.IsMatch(inputString)) {
+        if(reg.Matches(inputString)[0].ToString() == inputString) {
+            return true;
+        }
+    } 
+
+    return false;
+}
+
+/*--------------------------------------------------------------------*/
+/*--------------------------------------------------------------------*/
+
+// Shorter Solution
+
+bool isMAC48Address(string inputString) {
+    Regex reg = new Regex(@"[A-F\d-]{14}-[A-F\d]{2}");
+    if(reg.IsMatch(inputString)) if(reg.Matches(inputString)[0].ToString() == inputString) return true;
+
+    return false;
+}
