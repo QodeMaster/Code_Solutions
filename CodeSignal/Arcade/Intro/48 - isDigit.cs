4d9931d9435987f89f5db5f6ccb52f056bd301be
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-05-03 09:18:54 +0200

    Create 48 - isDigit.cs

---
+++ CodeSignal/Arcade/Intro/48 - isDigit.cs
@@ -0,0 +1,14 @@
+// Basic Solution
+
+bool isDigit(char symbol) {
+    if(Char.IsDigit(symbol)) return true;
+    else return false;
+}
+
+
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
+
+// One-liner
+
+bool isDigit1(char symbol) => Char.IsDigit(symbol);
