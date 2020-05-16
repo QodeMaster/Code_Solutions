8f3602cfd9f7db91bb6cee7d941ec950f3ef41dd
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-05-16 11:49:32 +0200

    Create 005 - Max_Multiple.cs

---
+++ CodeSignal/Arcade/The_Core/005 - Max_Multiple.cs
@@ -0,0 +1,12 @@
+// Basic Solution
+
+int maxMultiple(int divisor, int bound) {
+    return divisor * (bound / divisor);
+}
+
+/*--------------------------------------------------*/
+/*--------------------------------------------------*/
+
+// One-liner
+
+int maxMultiple(int divisor, int bound) => divisor * (bound / divisor);
