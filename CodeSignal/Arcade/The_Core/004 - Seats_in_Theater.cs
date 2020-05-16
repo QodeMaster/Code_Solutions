8efe650550903b65f0408873c18c960631d7d312
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-05-16 11:47:03 +0200

    Create 004 - Seats_in_Theater.cs

---
+++ CodeSignal/Arcade/The_Core/004 - Seats_in_Theater.cs
@@ -0,0 +1,13 @@
+// Basic Solution
+
+int seatsInTheater1(int nCols, int nRows, int col, int row) {
+    int affect = (nCols - col + 1) * (nRows - row);
+    return affect;
+}
+
+/*------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
+
+// One-liner
+
+int seatsInTheater(int nCols, int nRows, int col, int row) => (nCols - col + 1) * (nRows - row);
