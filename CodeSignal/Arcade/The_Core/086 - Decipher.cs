96ac4915275032f36338bf99dbd308fffa284224
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2020-08-20 09:02:02 +0200

    Update 086 - Decipher.cs

--- CodeSignal/Arcade/The_Core/086 - Decipher.cs
+++ CodeSignal/Arcade/The_Core/086 - Decipher.cs
@@ -1,4 +1,4 @@
-// Solution
+// Basic Solution
 
 string decipher(string cipher) {
     string decryp = "";
@@ -6,6 +6,26 @@ string decipher(string cipher) {
     int letter = 0;
     for(int i = 0; i < cipher.Length; i++) {
         letter = letter * 10 + (cipher[i] - '0');
+        if(96 < letter) {
+            decryp += (char)letter;
+            letter = 0;
+        }
+    }
+    
+    return decryp;
+}
+
+/*----------------------------------------------------------------*/
+/*----------------------------------------------------------------*/
+
+// Solution with foreach
+
+string decipher(string cipher) {
+    string decryp = "";
+    
+    int letter = 0;
+    foreach(char c in cipher) {
+        letter = letter * 10 + (c - '0');
         if(96 < letter) {
             decryp += (char)letter;
             letter = 0;
