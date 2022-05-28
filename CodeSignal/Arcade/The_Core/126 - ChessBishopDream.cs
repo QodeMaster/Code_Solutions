91d91b578ae909b08102782c91edb15517d1f514
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2022-05-28 13:53:32 +0200

    Added a new elegant solution

--- CodeSignal/Arcade/The_Core/126 - ChessBishopDream.cs
+++ CodeSignal/Arcade/The_Core/126 - ChessBishopDream.cs
@@ -33,3 +33,33 @@ int[] solution(int[] boardSize, int[] initPosition, int[] initDirection, int k) 
 
     return endCoord;
 }
+
+/*--------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------*/
+
+// A less repetitive and more elegant solution
+
+int[] solution(int[] boardSize, int[] initPosition, int[] initDirection, int k) {
+    // Final position coordinate
+    int[] endCoord = new int[2];
+    
+    for(int I = 0; I < 2; I++) {
+        int[] repeatingPattern = new int[3 * boardSize[I]];
+        int kI = k % (2 * boardSize[I]);
+        
+        // Filling pattern array
+        for(int i = 0; i < boardSize[I]; i++) {
+            // For the 'i' position in the example: [0, 1, 2, 2, 1, 0, 0, 1, 2];
+            //         'j' position in the example: [0, 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6];
+            repeatingPattern[i] = i;
+            repeatingPattern[i + 2 * boardSize[I]] = i;
+            repeatingPattern[i + boardSize[I]] = boardSize[I] - i - 1;
+        }
+        
+        // Calculate end position for the respective axis
+        endCoord[I] = repeatingPattern[initPosition[I] + initDirection[I] * kI + (1 - initDirection[I]) * boardSize[I]];
+    }
+    
+    // Return ending coordinates
+    return endCoord;
+}
